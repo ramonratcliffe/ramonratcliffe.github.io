62f29b54709ea8eba4441658279d84fd0ae506c1
alisonratcliffe93@gmail.com <68648399+ratcliffealison@users.noreply.github.com>
2025-03-06 19:54:17 +0100

    Fix loading and reloading

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'dist/index.html', dest: '' }, // Copy index.html to 404.html
+        { src: 'dist/index.html', rename: '404.html', dest: '' },
+      ],
+    }),
+  ],
   base: "/",
-})
+});
